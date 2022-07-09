c76374fdfef5d6ecc2f2a8e0726422963f2fe862
Кирилл <@HOME-PC>
2022-07-09 12:33:02 +1000

    Добавьте файлы проекта.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1. Необходимо написать приложение, которое определяет чётное или нечётное число.
+
+            Console.WriteLine("Введите целое число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.Write("Число является чётным");
+            }
+            else
+            {
+                Console.Write("Число является нечётным");
+            }
+        }
+    }
+}
